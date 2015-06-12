6c4c16d5be298629156db282e94ce0ac7fa4d315
Mike Overby <mikeoverby@outlook.com>
2015-06-12 05:56:51 -0700

    Now Searches both Draft and Official problems
    Broken into functions to make this easier to work with.

--- RosettaCodeNeedsX.py
+++ RosettaCodeNeedsX.py
@@ -3,39 +3,61 @@ __author__ = 'Mike'
 from urllib.request import *
 import re
 
-base_url = "http://rosettacode.org"
-category_url = base_url + "/wiki/Category:Draft_Programming_Tasks"
+BASE_URL = "http://rosettacode.org"
 
-request = Request(category_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
-response = urlopen(request)
-
-html = response.read().decode("utf-8") #decode bytes to a string
-
-#Collect links to the articles
-print("Collecting all drafts...")
-p = re.compile('<li><a href="(/wiki/(?:\w|_)+)"')
-articles = re.findall(p, html)
-
-#Collect links that do not have solutions in lang
-lang = input("What language do you want to contribute with? (exact spelling) ")
-
-print("Eliminating drafts with", lang, "solutions...")
-no_lang_sols = []
-for extension in articles:
-    art_url = base_url + extension
-    request = Request(art_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
+def collectLinks(arts, extension):
+    category_url = BASE_URL + extension
+    request = Request(category_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
     response = urlopen(request)
+    html = response.read().decode("utf-8") #decode bytes to a string
 
-    art_html = response.read().decode("utf-8")
-    if art_html.find('title="Edit section: ' + lang + '">') == -1:
-        no_lang_sols.append(extension)
+    #Collect links to the articles
+    p = re.compile('<li><a href="(/wiki/(?:\w|_)+)"')
+    arts.extend(re.findall(p, html))
 
-print()
-print("There are", len(no_lang_sols), "draft tasks without Python solutions.")
-for art in no_lang_sols:
-    print("    " + art.replace("_", " ")[6:] + "    (" + base_url+art + ")")
+def filterProblems(arts, lang):
 
-with open(lang + "Problems.txt", "w") as f:
-    f.write("There are " + str(len(no_lang_sols)) + " draft tasks without Python solutions.\\n")
-    for art in no_lang_sols:
-        f.write("    " + art.replace("_", " ")[6:] + "    (" + base_url+art + ")\\n")
+    print("Eliminating tasks with", lang, "solutions...")
+    t = len(arts)
+    no_lang_sols = []
+    for extension in arts:
+        print(t)
+        t -= 1
+        art_url = BASE_URL + extension
+        request = Request(art_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
+        response = urlopen(request)
+
+        art_html = response.read().decode("utf-8")
+        if art_html.find('title="Edit section: ' + lang + '">') == -1:
+            no_lang_sols.append(extension)
+
+    return no_lang_sols
+
+
+def outputResults(results, lang):
+    print()
+    print("There are", len(results), "tasks without Python solutions.")
+    for art in results:
+        print("    " + art.replace("_", " ")[6:] + "    (" + BASE_URL+art + ")")
+
+    with open(lang + "Problems.txt", "w") as f:
+        f.write("There are " + str(len(results)) + " tasks without Python solutions.\\n")
+        for art in results:
+            f.write("    " + art.replace("_", " ")[6:] + "    (" + BASE_URL+art + ")\\n")
+
+
+def main():
+    articles = []
+    print("Collecting all official tasks...")
+    collectLinks(articles, "/wiki/Category:Programming_Tasks")
+    print("Collecting all draft tasks...")
+    collectLinks(articles, "/wiki/Category:Draft_Programming_Tasks")
+
+    #Collect links that do not have solutions in lang
+    lang = input("What language do you want to contribute with? (exact spelling) ")
+
+    results = filterProblems(articles, lang)
+
+    outputResults(results, lang)
+
+main()
