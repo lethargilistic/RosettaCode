a19ad666d63fdd02684d989d5ff76387788c1763
Mike Overby <mikeoverby@outlook.com>
2015-06-13 09:07:00 -0700

    Improved speed by factor of 60
    Instead of going to each page and looking for the language's entry, I now look at the Category:<Language> page. That has links to all solved problems in that language. This lowered my number of HTTP requests for a Python test from 650+ to 2.
    
    Instead of storing the article extensions in lists, I now use sets so that I can just do a difference operation.
    
    I also broke off getting the HTML string into its own function.

--- RosettaCodeNeedsX.py
+++ RosettaCodeNeedsX.py
@@ -3,33 +3,43 @@ __author__ = 'Mike'
 from urllib.request import *
 import re
 
-BASE_URL = "http://rosettacode.org"
 
-def collectLinks(arts, extension):
-    category_url = BASE_URL + extension
-    request = Request(category_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
+BASE_URL = "http://rosettacode.org"
+ARTICLE_PATTERN = re.compile('<li><a href="(/wiki/(?:\w|_)+)"')
+
+
+def readPageHTML(url_extension):
+    url = BASE_URL + url_extension
+    request = Request(url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
     response = urlopen(request)
     html = response.read().decode("utf-8") #decode bytes to a string
+    return html
+
+
+def collectLinks(arts, extension):
+    html = readPageHTML(extension)
 
     #Collect links to the articles
-    p = re.compile('<li><a href="(/wiki/(?:\w|_)+)"')
-    arts.extend(re.findall(p, html))
+    arts.extend(re.findall(ARTICLE_PATTERN, html))
+
 
 def filterProblems(arts, lang):
-
     print("Eliminating tasks with", lang, "solutions...")
-    t = len(arts)
-    no_lang_sols = []
-    for extension in arts:
-        print(t)
-        t -= 1
-        art_url = BASE_URL + extension
-        request = Request(art_url, headers={"User-Agent" : "Magic Browser"}) #Spoofing as a browser
-        response = urlopen(request)
 
-        art_html = response.read().decode("utf-8")
-        if art_html.find('title="Edit section: ' + lang + '">') == -1:
-            no_lang_sols.append(extension)
+    lang_html = readPageHTML("/wiki/Category:" + lang)
+
+    #Find index in string where the link section starts
+    start = lang_html.find("Pages in category")
+
+    #Collect the links
+    yes_lang_sols = set(re.findall(ARTICLE_PATTERN, lang_html[start:]))
+
+    #Get all links that are in arts, but not in yes_lang_sols
+    #These are the problems for which there is no solution in the language
+    no_lang_sols = list(arts.difference(yes_lang_sols))
+
+    #Sort list in alphabetical order
+    no_lang_sols.sort()
 
     return no_lang_sols
 
@@ -41,23 +51,28 @@ def outputResults(results, lang):
         print("    " + art.replace("_", " ")[6:] + "    (" + BASE_URL+art + ")")
 
     with open(lang + "Problems.txt", "w") as f:
-        f.write("There are " + str(len(results)) + " tasks without Python solutions.\\n")
+        f.write("There are " + str(len(results)) + " tasks without Python solutions.\n")
         for art in results:
-            f.write("    " + art.replace("_", " ")[6:] + "    (" + BASE_URL+art + ")\\n")
+            f.write("    " + art.replace("_", " ")[6:] + "    (" + BASE_URL+art + ")\n")
 
 
 def main():
+    #Extend a list with all tasks on RosettaCode
     articles = []
     print("Collecting all official tasks...")
     collectLinks(articles, "/wiki/Category:Programming_Tasks")
     print("Collecting all draft tasks...")
     collectLinks(articles, "/wiki/Category:Draft_Programming_Tasks")
 
-    #Collect links that do not have solutions in lang
-    lang = input("What language do you want to contribute with? (exact spelling) ")
+    #Convert that list into a set. (Can't extend a set, so a list was used for initial populating)
+    articles = set(articles)
 
+    #Do the filtering for a language
+    lang = input("What language do you want to contribute with? (exact spelling) ")
     results = filterProblems(articles, lang)
 
+    #Output the results
     outputResults(results, lang)
+
 
 main()
